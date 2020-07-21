486977ed20fd86f865fe4ac9ccbd7ed132f861c1
U1finator <owerning@online.de>
2020-07-21 13:55:15 +0000

    Updated the Read.Me to warn about the broken Badge

--- Main.java
+++ Main.java
@@ -1,14 +1,15 @@
-class Main
+class Main 
 {
-  //main-Mathode um das Programm zu starten
-  public static void main(String[] args) {
-        String plaintext = "Guten Tag!";
-        int key = 3;
+  // main-Mathode um das Programm zu starten
+  public static void main(String[] args)
+  {
+    String plaintext = "Guten Tag!";
+    int key = 3;
 
-        String ciphertext = code(plaintext, key);
-        System.out.println("Klartext: " + plaintext + " ---> " + ciphertext);
+    String ciphertext = code(plaintext, key);
+    System.out.println("Klartext: " + plaintext + " ---> " + ciphertext);
 
-        String decoded = decode(ciphertext, key);
-        System.out.println("Geheimtext: " + ciphertext + " ---> " + decoded);
-    }
+    String decoded = decode(ciphertext, key);
+    System.out.println("Geheimtext: " + ciphertext + " ---> " + decoded);
+  }
 }
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Eine Möglichkeit Texte zu verschlüsseln und wieder dechiffrieren
 
 Es wird hierbei mit einem symmetrischen Verfahren gearbeitet und es ist angedacht mehrere Verschlüsselungsebenen zu verwenden (Überschlüsselung).
 
-
+!!wichtig!!: die Badge für Repl.it funktioniert nicht
 
 für die Seite Repl.it: 
 [![Run on Repl.it](https://repl.it/badge/github/U1finator/Verschl-sselung)](https://repl.it/github/U1finator/Verschl-sselung)
